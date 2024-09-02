435494dbdd5312b965d695b147d934e7a2896f3a
Dhanurgarg <141549983+Dhanurgarg@users.noreply.github.com>
2024-09-03 00:15:02 +0530

    main.cpp

--- main.cpp
+++ main.cpp
@@ -218,7 +218,7 @@ int main()
 		case 3:
 			cout << "Enter amount to withdrawn : ";
 			cin >> amount;
-			person.Withdraw(amount);
+			ans=person.Withdraw(amount);
 
 			if (ans)
 				cout << "Successfully withdrawn Amount"
